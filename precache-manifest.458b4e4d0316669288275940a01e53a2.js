93650d929a1b25c4c9331d7a70fdc2cd5f55ba03
Austin Cory Bart <acbart@vt.edu>
2020-01-21 07:15:10 -0500

    Updates

---
+++ precache-manifest.458b4e4d0316669288275940a01e53a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7ddc88400ade3cabaaa9596d145777",
+    "url": "/phaser-test-game/assets/img/ada.png"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "/phaser-test-game/assets/img/phaser-logo.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "/phaser-test-game/favicon.ico"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "/phaser-test-game/icons/icons-192.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "/phaser-test-game/icons/icons-512.png"
+  },
+  {
+    "revision": "65a6c5a42972c0171d4775fd4f5a8a6f",
+    "url": "/phaser-test-game/index.html"
+  },
+  {
+    "revision": "ef52d360c60a70e7e9e5",
+    "url": "/phaser-test-game/main.fc6384c4fad8d62b1a3f.bundle.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "/phaser-test-game/manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/phaser-test-game/sw.js"
+  },
+  {
+    "revision": "4224705d2fb2af9fc927",
+    "url": "/phaser-test-game/vendors.bed8fb786309aec76dee.bundle.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,11 @@
+importScripts("/phaser-test-game/precache-manifest.458b4e4d0316669288275940a01e53a2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
